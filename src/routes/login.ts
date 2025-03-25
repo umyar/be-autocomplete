5e25a8f26d2ca3b8b115cb0d20834e9cc67d13ac
Umiar Iusupov <umiar.iusupov@deel.com>
2025-03-25 09:46:39 +0300

    json response back (after the FE fixes)

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -33,7 +33,7 @@ loginRouter.post('/login', (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.status(401).send(EMPTY_CREDS_ERROR.message);
+    return res.status(401).json(EMPTY_CREDS_ERROR);
   }
 
   const isCredentialsValid = checkCredentials(email, password);
@@ -41,7 +41,7 @@ loginRouter.post('/login', (req, res) => {
   if (isCredentialsValid) {
     return res.status(200).json(VALID_CREDS_MSG);
   } else {
-    return res.status(401).send(INVALID_CREDS_ERROR.message);
+    return res.status(401).json(INVALID_CREDS_ERROR);
   }
 });
 
